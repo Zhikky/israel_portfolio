f9de3fb18b02258b17525e938c38d29b208b33d4
Ezekiel Adetuwo <ezeadetuwo@gmail.com>
2025-12-25 10:47:05 +0100

    updates x icon in Image popup container

--- src/Component/composables/imageCardPopup.jsx
+++ src/Component/composables/imageCardPopup.jsx
@@ -8,8 +8,22 @@ export default function ImageCardPopup({ hideImagePopup, imageData }) {
       className="fixed inset-0 w-full h-screen flex flex-col items-center justify-center bg-black/50 backdrop-blur-sm z-[9999] cursor-pointer"
       onClick={hideImagePopup}
     >
-      <div className="text-2xl px-5 py-3 text-black bg-white rounded-full mb-10 hover:bg-[#94E1CA] cursor-pointer hover:scale-105 hover:text-white">
-        X
+      <div className="text-2xl p-3 text-black bg-white rounded-full mb-10 hover:bg-[#94E1CA] cursor-pointer hover:scale-105 hover:text-white">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18 6L6 18M6 6L18 18"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
       <img
         src={imageData}
